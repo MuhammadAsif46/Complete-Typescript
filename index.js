a196e203703b36127f7838990e44f7ba40ff0dcd
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-06-16 01:44:33 +0500

    convert js

---
+++ index.js
@@ -0,0 +1,11 @@
+// primitive Types:
+// 1. String:
+var greetings = "Hello, world!";
+greetings.toLowerCase();
+console.log(greetings);
+// 2. Number:
+var userId = 3322122;
+console.log(userId.toFixed());
+// 3. Boolean:
+var isLoggedIn = false;
+console.log(isLoggedIn);
