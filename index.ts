87261c994549eddeb0a5efd7d40dbd693a0f5cd5
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-06-16 00:42:07 +0500

    added some types

--- index.ts
+++ index.ts
@@ -1,6 +1,22 @@
+// primitive Types:
+
+// 1. String:
+
 let greetings: string = "Hello, world!";
-let num = 4;
 greetings.toLowerCase()
 console.log(greetings);
+
+
+// 2. Number:
+
+let userId: number = 3322122;
+console.log(userId.toFixed());
+
+// 3. Boolean:
+
+let isLoggedIn: boolean = false;
+console.log(isLoggedIn);
+
+
 
 
