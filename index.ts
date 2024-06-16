1d42901c69ebc51609dc63d1a5f307953fd9a1db
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-06-16 13:15:06 +0500

    function how to write and call

--- index.ts
+++ index.ts
@@ -17,6 +17,20 @@ console.log(userId.toFixed());
 let isLoggedIn: boolean = false;
 console.log(isLoggedIn);
 
+// TODO 1 : Write a program to add two numbers?
+// TODO 2 : How to catch Error and solve it?
+// TODO 3 : TS  Configuration file
+
+// func define:
+
+function sum(a:number,b:number):number {
+    return a + b;
+}
+
+// func call:
+console.log(sum(1,2));
 
 
+
+export {}
 
