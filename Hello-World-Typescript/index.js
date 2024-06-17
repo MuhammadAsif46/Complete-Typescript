65fc48bd804186772a5d490be1ce6613808a13e6
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-06-17 12:26:53 +0500

    simple hello world

--- Hello-World-Typescript/index.js
+++ Hello-World-Typescript/index.js
@@ -1,6 +1,4 @@
-"use strict";
 // primitive Types:
-Object.defineProperty(exports, "__esModule", { value: true });
 // 1. String:
 var greetings = "Hello, world!";
 greetings.toLowerCase();
@@ -11,12 +9,3 @@ console.log(userId.toFixed());
 // 3. Boolean:
 var isLoggedIn = false;
 console.log(isLoggedIn);
-// TODO 1 : Write a program to add two numbers?
-// TODO 2 : How to catch Error and solve it?
-// TODO 3 : TS  Configuration file
-// func define:
-function sum(a, b) {
-    return a + b;
-}
-// func call:
-console.log(sum(1, 2));
