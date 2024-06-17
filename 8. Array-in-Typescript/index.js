b44cd05b9c31bae3f378cd03cf24987c8abed820
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-06-17 17:19:40 +0500

    added array using typescript

---
+++ 8. Array-in-Typescript/index.js
@@ -0,0 +1,12 @@
+// Array in Typescript:
+// In Typescript, you can create and initialize arrays using various approaches.
+// a) Using square brackets:
+var number = [1, 2, 3, 4, 5];
+var str = ["a", "b", "c", "d", "e"];
+// b) Using the array constructor:
+var number1 = new Array(1, 2, 3, 4, 5);
+var str1 = new Array("a", "b", "c", "d", "e");
+// c) Using the Array.of method:
+var names = Array.of("a", "b", "c", "d");
+// Operation: Accessing elements using index:-
+console.log(names[1]);
---
+++ 8. Array-in-Typescript/index.ts
@@ -0,0 +1,22 @@
+// Array in Typescript:
+
+// In Typescript, you can create and initialize arrays using various approaches.
+
+// a) Using square brackets:
+
+const number: number[] = [1, 2, 3, 4, 5];
+const str: string[] = ["a", "b", "c", "d", "e"];
+
+// b) Using the array constructor:
+
+const number1: number[] = new Array(1, 2, 3, 4, 5);
+const str1: string[] = new Array("a", "b", "c", "d", "e");
+
+
+// c) Using the Array.of method:
+
+const names: string[] = Array.of("a", "b", "c", "d");
+
+// Operation: Accessing elements using index:-
+
+console.log(names[1]);
