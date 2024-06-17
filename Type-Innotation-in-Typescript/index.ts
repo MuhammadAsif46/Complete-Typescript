3fa70720e5c533b722c29292d835018a3aa85c43
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-06-17 13:28:04 +0500

    complete number types

--- Type-Innotation-in-Typescript/index.ts
+++ Type-Innotation-in-Typescript/index.ts
@@ -1,7 +1,27 @@
 /*  In Typescript, type annotation is a way of explicitly specifying
 the type of a variable, function parameters or function return value. */
 
+let myFavNum: number = 10;
+let myName: string = "Jhon";
+let isLoggedIn: boolean = false;
 
-let myFavNum:number = 10;
-let myFullName:string = "Jhon";
-let myF:number = 10;
+// Number Type:
+
+let myNumber: number = 5;
+let myAge: number = 20;
+let pi: number = 3.123;
+let myNegVal: number = -8;
+
+// String Type:
+
+let myFirstName: string = "Asif"
+let myLastName: string = 'Ahmed'
+let myFullName =  myFirstName + " " + myLastName
+console.log(myFullName);
+
+// create a simple string variable in a sentence and find the length of the string variable?
+
+let sentence:string = "My first sentence is completed"
+console.log(sentence.length);
+
+
