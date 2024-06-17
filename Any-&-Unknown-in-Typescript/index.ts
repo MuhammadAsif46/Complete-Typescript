5e9316517f698fe8a834a4574d30a1c3493bc29a
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-06-17 14:47:38 +0500

    added unknown type

---
+++ Any-&-Unknown-in-Typescript/index.ts
@@ -0,0 +1,34 @@
+// Any Type:
+
+let num: any = 5;
+num = "hello";
+num = false;
+
+// Unknown Type:
+// type checking if number then this and string then this
+
+let num2:unknown;
+num2 = 4;
+num2 = "user";
+num2 = false;
+
+if (typeof num2 === "number") {
+    console.log(num2 + 2);
+} else if (typeof num2 === "boolean") {
+    console.log(num2);    
+}
+
+// async function fetchData(): Promise<unknown>{
+//     const response = await fetch("https://api.example.com/data");
+//     const data = await response.json();
+//     return data;
+// }
+
+// async function processData(){
+//     const response = await fetchData();
+
+//     if (typeof response === "object") {
+//         console.log("response ->", response);
+        
+//     }
+// }
--- Any-&-Unkown-in-Typescript/index.ts
+++
@@ -1,7 +0,0 @@
-// Any Type:
-
-let num: any = 5;
-num = "hello";
-num = false;
-
-
