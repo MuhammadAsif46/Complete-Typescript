2f2856294345c821c9bb9ab12e10975a5a50485e
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-06-17 13:00:56 +0500

    adding types in typescript

---
+++ Type-Innotation-in-Typescript/index.ts
@@ -0,0 +1,7 @@
+/*  In Typescript, type annotation is a way of explicitly specifying
+the type of a variable, function parameters or function return value. */
+
+
+let myFavNum:number = 10;
+let myFullName:string = "Jhon";
+let myF:number = 10;
