46d8217e43db2d4c3202cd3c76c1583f7bb4927e
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-06-17 13:37:01 +0500

    added string type and its 2 task

--- Type-Innotation-in-Typescript/index.js
+++ Type-Innotation-in-Typescript/index.js
@@ -1,38 +1,26 @@
 /*  In Typescript, type annotation is a way of explicitly specifying
 the type of a variable, function parameters or function return value. */
-
-let myFavNum: number = 10;
-let myName: string = "Jhon";
-let isLoggedIn: boolean = false;
-
+var myFavNum = 10;
+var myName = "Jhon";
+var isLoggedIn = false;
 // Number Type:
-
-let myNumber: number = 5;
-let myAge: number = 20;
-let pi: number = 3.123;
-let myNegVal: number = -8;
-
+var myNumber = 5;
+var myAge = 20;
+var pi = 3.123;
+var myNegVal = -8;
 // String Type:
-
-let myFirstName: string = "Asif"
-let myLastName: string = 'Ahmed'
-let myFullName =  myFirstName + " " + myLastName
+var myFirstName = "Asif";
+var myLastName = 'Ahmed';
+var myFullName = myFirstName + " " + myLastName;
 console.log(myFullName);
-
 // String Task 1:
 // create a simple string variable in a sentence and find the length of the string variable?
-
-let sentence:string = "My first sentence is completed";
-let sentenceLength:number = sentence.length;
+var sentence = "My first sentence is completed";
+var sentenceLength = sentence.length;
 console.log(sentenceLength);
-
 // String Task 2:
 // create a simple 2 string variables to to convert in upper and lower case letters;
-
-let upperCase:string = "hello world"
+var upperCase = "hello world";
 console.log(upperCase.toUpperCase());
-
-let lowerCase:string = "WORLD HELLO"
+var lowerCase = "WORLD HELLO";
 console.log(lowerCase.toLowerCase());
-
-
