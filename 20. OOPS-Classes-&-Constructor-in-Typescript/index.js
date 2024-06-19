fca624c5dff92a70d83cd8e8008fd148c181d20d
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-06-19 12:35:23 +0500

    added construstors in TS

--- 20. OOPS-Classes-&-Constructor-in-Typescript/index.js
+++ 20. OOPS-Classes-&-Constructor-in-Typescript/index.js
@@ -3,13 +3,16 @@
 // A class is like a blueprint for creating similar things:
 // create class in TS:
 var Persons = /** @class */ (function () {
-    function Persons() {
-        this.name = "Jhon";
-        this.age = 19;
-        this.hobbies = ["reading", "writing"];
+    // create a constructor:
+    function Persons(name, age, hobbies) {
+        this.name = name;
+        this.age = age;
+        this.hobbies = hobbies;
     }
     return Persons;
 }());
 // Intance of class in TS:
-var persons1 = new Persons();
-console.log(persons1);
+var persons1 = new Persons("Jhon", 19, ["reading", "writing"]);
+var persons2 = new Persons("Elia", 21, ["coding", "palying"]);
+var persons3 = new Persons("Smith", 31, ["leaning", "sleeping"]);
+console.log(persons1.hobbies);
--- 20. OOPS-Classes-&-Constructor-in-Typescript/index.ts
+++ 20. OOPS-Classes-&-Constructor-in-Typescript/index.ts
@@ -6,12 +6,19 @@
 // create class in TS:
 
 class Persons {
-  name: string = "Jhon";
-  age: number = 19;
-  hobbies: string[] = ["reading","writing"];
+  name: string;
+  age: number;
+  hobbies: string[];
+  // create a constructor:
+  constructor(name: string, age: number, hobbies: string[]) {
+    this.name = name;
+    this.age = age;
+    this.hobbies = hobbies;
+  }
 }
 
 // Intance of class in TS:
-const persons1:Persons = new Persons();
-console.log(persons1);
-
+const persons1: Persons = new Persons("Jhon", 19, ["reading", "writing"]);
+const persons2: Persons = new Persons("Elia", 21, ["coding", "palying"]);
+const persons3: Persons = new Persons("Smith", 31, ["leaning", "sleeping"]);
+console.log(persons1.hobbies);
